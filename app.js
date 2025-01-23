f13596de8f2fd03197b1cdfaf77c7a5bf1fab1c5
Robinson <nrobinson4@una.edu>
2025-01-23 16:27:09 -0600

    added get and post forms with ejs

--- app.js
+++ app.js
@@ -4,8 +4,11 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser')
 
 /** uses index.html in the public directory */
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + "/public"))
 
 /** where the actual web thingy gets pointed from codepen example
@@ -20,7 +23,27 @@ app.get('/ejs', function (req, res) {
     {pageTitle: 'my cool ejs page'}
   );
 })
- 
+
+app.get('/saveMyNameGet', (req, res) => {
+  console.log('did we hit the get endpoint')
+  
+  console.log(req.query)
+  
+  res.redirect('/ejs')
+})
+
+app.post('/saveMyNamePost', (req, res) => {
+  console.log('did we hit the post endpoint')
+  
+  console.log(req.body)
+  
+  // res.redirect('/ejs')
+  res.render('words', 
+    {pageTitle: req.body.myName}
+  );
+})
+
+
 app.get('/nodemon', function (req, res) {
   res.send('AHHHHHHHHHHHHHHH')
 })
@@ -33,6 +56,7 @@ app.get('/nodemon', function (req, res) {
 app.get('/helloRender', function (req, res) {
   res.send('Hello Express from real world<br><a href="/"> back to home</a>')
 })
+
 
 
 // IIFE immediately invoked function expression
